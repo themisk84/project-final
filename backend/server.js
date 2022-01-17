188e41f692778c93d4753f295d85e07585377eb8
themisk84 <efthymios.karakasis@gmail.com>
2022-01-17 15:25:37 +0100

    feat(backend): adding routes

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,197 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cloudinaryFramework from "cloudinary";
+import multer from "multer";
+import cloudinaryStorage from "multer-storage-cloudinary";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/finalProject";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
+dotenv.config();
+
+const cloudinary = cloudinaryFramework.v2;
+cloudinary.config({
+  cloud_name: "dmolotv8a",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "images",
+    allowedFormats: ["jpg", "png"],
+    transformation: [{ width: 500, height: 500, crop: "limit" }],
+  },
+});
+const parser = multer({ storage });
+
+// model for activities
+const SightSeeingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+    enum: ["Sweden", "Norway", "Denmark"],
+  },
+  imageUrl: String,
+  createdAt: {
+    type: Number,
+    default: Date.now,
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 140,
+    minlength: 5,
+    required: true,
+  },
+});
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: (v) => {
+        let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        const result = re.test(v);
+        return result;
+      },
+      message: "Please fill a valid email address",
+    },
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+const Sightseeing = mongoose.model("Sightseeing", SightSeeingSchema);
+const User = mongoose.model("User", UserSchema);
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res
+        .status(401)
+        .json({ response: "User not found, please login", success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+};
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.post("/sightseeing", parser.single("image"), async (req, res) => {
+  try {
+    const newSightseeing = await new Sightseeing({
+      name: req.body.name,
+      country: req.body.country,
+      imageUrl: req.file.path,
+    }).save();
+    res.json({
+      response: newSightseeing,
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({ errors: err.errors, success: false });
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const { username, password, email } = req.body;
+  try {
+    const salt = bcrypt.genSaltSync();
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password, salt),
+      email,
+    }).save();
+
+    res.status(201).json({
+      response: {
+        userId: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        accessToken: newUser.accessToken,
+      },
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error, success: false });
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        response: {
+          userId: user._id,
+          username: user.username,
+          accessToken: user.accessToken,
+        },
+        success: true,
+      });
+    } else {
+      res.status(404).json({
+        response: "Username or password doesn't match",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+// app.get("/user", authenticateUser);
+// app.get("/user", async (req, res) => {});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
