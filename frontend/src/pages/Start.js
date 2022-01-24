6ab18b2450c2474a3b473d4a58c007536d1a4330
elsisco <elsa.carlstrom@gmail.com>
2022-01-24 15:00:26 +0100

    added country options

--- frontend/src/pages/Start.js
+++ frontend/src/pages/Start.js
@@ -1,29 +1,34 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { useSelector, useDispatch, batch } from "react-redux";
-import sightseeing from "../reducers/sightseeing";
-import Navbar from "../components/Navbar";
-import Searchbar from "../components/Searchbar";
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { useSelector, useDispatch, batch } from 'react-redux'
+import sightseeing from '../reducers/sightseeing'
+import Navbar from '../components/Navbar'
+import Searchbar from '../components/Searchbar'
 
 const Start = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   useEffect(() => {
-    fetch("https://go-scandinavia.herokuapp.com/stories")
+    fetch('https://go-scandinavia.herokuapp.com/stories')
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        const info = data.response;
-        const info2 = data.response.map((item) => item);
-        console.log(info2);
+        console.log(data)
+        const info = data.response
+        const info2 = data.response.map((item) => item)
+        console.log(info2)
         if (data.success) {
-          dispatch(sightseeing.actions.addSightseeing(data.response));
+          info.map((item) => {
+            console.log(item)
+            //dispatch(sightseeing.actions.addSightseeing(item));
+          })
+        } else {
         }
-      });
-  }, []);
+      })
+  }, [])
 
   // const sightseeingSearch = useSelector(
   //   (store) => store.sightseeing.sightseeings
   // );
+  const cities = ['Norway', 'Sweden', 'Denmark']
 
   return (
     <StyledHero>
@@ -41,40 +46,114 @@ const Start = () => {
           aliquip ex ea commodo consequat."
         </StyledParagraph>
         <Searchbar />
+        <StyledContainerCountry>
+          {cities.map((item) => (
+            <StyledCountryWrapper>
+              <StyledCountry item={item}></StyledCountry>
+              <StyledTitle item={item}>{item}</StyledTitle>
+            </StyledCountryWrapper>
+          ))}
+        </StyledContainerCountry>
         {/* <button onClick={()=> setCountry("Sweden")}>Sweden</button>
         <button>Denmark</button>
         <button>Norway</button> */}
       </StyledContainer>
     </StyledHero>
-  );
-};
+  )
+}
 
-export default Start;
+export default Start
 
 const StyledHero = styled.div`
-  background-image: url("/assets/background.png");
+  background-image: url('/assets/background.png');
   background-size: cover;
   background-repeat: no-repeat;
   background-position: right;
   width: 100%;
-  height: 100%;
+  height: 110%;
   display: flex;
   flex-direction: column;
   position: absolute;
-`;
+`
+
+const StyledHeader = styled.nav`
+  width: 100%;
+  height: 70px;
+  backdrop-filter: blur(12px);
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 25px;
+`
+const StyledHamburger = styled.div`
+  display: flex;
+  width: 25px;
+  height: 25px;
+  flex-direction: column;
+  justify-content: space-between;
+
+  div {
+    width: 30px;
+    height: 3px;
+    background-color: #56baa0;
+    border-radius: 5px;
+  }
+`
 
 const StyledHeadline = styled.h1`
   color: white;
   margin: 2px;
   font-size: 40px;
-`;
+`
 
 const StyledContainer = styled.div`
   margin-top: 100px;
   padding: 0 25px 25px 25px;
-`;
+`
 const StyledParagraph = styled.p`
   color: white;
   line-height: 1.3;
   font-size: 14px;
-`;
+`
+
+const StyledContainerCountry = styled.div`
+  display: flex;
+  margin: 10px 0;
+  overflow: auto;
+  border: 1px solid red;
+  width: 1000px;
+  flex-wrap: wrap;
+  div {
+    display: flex;
+    text-align: center;
+  }
+`
+const StyledCountryWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+`
+const StyledCountry = styled.div`
+  height: 250px;
+  background-size: cover;
+  border-radius: 12px;
+  ${(props) =>
+    props.item === 'Norway'
+      ? `background-image: url('/assets/norway.jpg')`
+      : props.item === 'Sweden'
+      ? `background-image:url('/assets/sweden.jpg')`
+      : `background-image:url('/assets/denmark.jpg')`}
+`
+const StyledTitle = styled.p`
+  text-align: center;
+  font-weight: bold;
+  text-transform: uppercase;
+
+  ${(props) =>
+    props.item === 'Norway'
+      ? `color: #56baa0`
+      : props.item === 'Sweden'
+      ? `color: #638eca`
+      : `color: #bedaa8`}
+`
