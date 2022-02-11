f1d97cf0857db5b53d986613d15bc526eb8af7b6
elsisco <elsa.carlstrom@gmail.com>
2022-02-11 07:30:10 +0100

    small change to the svaed posts layout

--- frontend/src/pages/SavedPosts.js
+++ frontend/src/pages/SavedPosts.js
@@ -1,11 +1,11 @@
-import React from "react";
-import styled from "styled-components";
-import { useSelector } from "react-redux";
-import { FaRegCompass } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React from 'react'
+import styled from 'styled-components'
+import { useSelector } from 'react-redux'
+import { FaRegCompass } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const UserPage = () => {
-  const saved = useSelector((store) => store.user?.savedSights);
+  const saved = useSelector((store) => store.user?.savedSights)
 
   return (
     <>
@@ -24,35 +24,36 @@ const UserPage = () => {
                     <InfoWrapper>
                       <FaRegCompass
                         style={{
-                          marginRight: "6",
-                          height: "14",
+                          marginRight: '6',
+                          height: '14',
                         }}
                       />
                       <p
                         style={{
-                          fontSize: "14px",
-                          fontStyle: "italic",
-                        }}
-                      >
+                          fontSize: '14px',
+                          fontStyle: 'italic',
+                        }}>
                         {item.location}
                       </p>
                     </InfoWrapper>
                     <InfoWrapper>
-                      <p>Category:</p>
-                      <p style={{ marginLeft: 5 }}>{item.category}</p>
+                      <p style={{ margin: 0 }}>Category:</p>
+                      <p style={{ margin: 0, marginLeft: 5 }}>
+                        {item.category}
+                      </p>
                     </InfoWrapper>
                   </InfoContainer>
                 </LikedPostWrapper>
               </StyledLink>
             </>
-          );
+          )
         })}
       </AttractionContainer>
     </>
-  );
-};
+  )
+}
 
-export default UserPage;
+export default UserPage
 
 const AttractionContainer = styled.div`
   display: flex;
@@ -61,9 +62,13 @@ const AttractionContainer = styled.div`
   width: 100%;
   @media (min-width: 768px) {
     flex-direction: row;
+    justify-content: flex-start;
     flex-wrap: wrap;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0;
   }
-`;
+`
 const StyledHeadline = styled.h2`
   color: #061137;
   margin: 100px auto 0 auto;
@@ -71,53 +76,50 @@ const StyledHeadline = styled.h2`
   @media (min-width: 768px) {
     margin: 200px auto 50px auto;
   }
-`;
+`
 
 const StyledLink = styled(Link)`
-  /* text-decoration: none;
-  color: black; */
   border-radius: 20px;
-  width: 300px;
+  width: 100%;
   height: 150px;
   margin-bottom: 20px;
   overflow: hidden;
   display: flex;
-  background-color: white;
   text-decoration: none;
   color: black;
   margin: 10px auto;
   @media (min-width: 768px) {
-    width: 400px;
+    width: 31%;
   }
-`;
+`
 
 const LikedPostWrapper = styled.div`
   border-radius: 20px;
-  width: 300px;
+  width: 100%;
   height: 150px;
   margin-bottom: 20px;
   overflow: hidden;
   display: flex;
   background-color: white;
-`;
+`
 const ImageContainer = styled.div`
   height: 100%;
   width: 45%;
   background-image: ${(props) => `url(${props.image})`};
   background-size: cover;
   background-position: center;
-`;
+`
 const InfoContainer = styled.div`
   background-color: white;
   border-radius: 20px;
   margin-left: -20px;
   padding: 15px;
-`;
+`
 const ActivityName = styled.h3`
   margin: 0;
   margin-top: 10px;
-`;
+`
 const InfoWrapper = styled.div`
   display: flex;
   align-items: center;
-`;
+`
