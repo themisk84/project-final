c21a1147c54d6454b257ce73f3851523f40ce83f
Никитина Юля <nik.iuliiasw@gmail.com>
2022-02-14 21:08:29 +0100

    fixed reducer user.js for savedSights

--- frontend/src/reducers/user.js
+++ frontend/src/reducers/user.js
@@ -24,29 +24,34 @@ const user = createSlice({
     setError: (store, action) => {
       store.error = action.payload;
     },
-    // addSavedPost: (store, action) => {
-    //   console.log(action.payload);
-    //   if (store.savedSights?.length === 0) {
-    //     store.savedSights = [action.payload];
-    //   } else
-    //     store.savedSights?.length > 0 &&
-    //       store.savedSights.map((item) => {
-    //         if (item._id === action.payload._id) {
-    //           return store.savedSights;
-    //         } else {
-    //           return action.payload;
-    //         }
-    //       });
-    //   store.savedSights = [...store.savedSights, action.payload];
-    // },
     addSavedPost: (store, action) => {
+      console.log(action.payload);
       if (store.savedSights?.length === 0) {
         store.savedSights = [action.payload];
       } else if (store.savedSights?.length > 0) {
-        console.log(store.savedSights);
-        store.savedSights = [...store.savedSights, action.payload];
+        const existingPost = store.savedSights.find((item) => item._id === action.payload._id)
+        if (!existingPost) {
+          store.savedSights = [...store.savedSights, action.payload]
+        }
       }
+      // store.savedSights.map((item) => {
+      //   if (item._id === action.payload._id) {
+      //     return item;
+      //   } else {
+      //     return store.savedSights = [...store.savedSights, action.payload];
+      //     console.log([...store.savedSights, action.payload])
+      //   }
+      // });
+      // store.savedSights = [...store.savedSights, action.payload];
     },
+    // addSavedPost: (store, action) => {
+    //   if (store.savedSights?.length === 0) {
+    //     store.savedSights = [action.payload];
+    //   } else if (store.savedSights?.length > 0) {
+    //     console.log(store.savedSights);
+    //     store.savedSights = [...store.savedSights, action.payload];
+    //   }
+    // },
 
     deleteSavedPost: (store, action) => {
       const deletedSavedPost = store.savedSights.filter(
