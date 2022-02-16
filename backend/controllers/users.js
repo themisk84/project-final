206914f7edb0101b2d7c6a0bd8884b05463af781
elsisco <elsa.carlstrom@gmail.com>
2022-02-16 20:31:29 +0100

    small fixes and added readme

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,20 +1,19 @@
-const express = require("express");
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-import crypto from "crypto";
+const express = require('express')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
-const userRouter = express.Router();
+const userRouter = express.Router()
 
-userRouter.post("/signup", async (req, res) => {
-  const { username, password, email, avatar } = req.body;
+userRouter.post('/signup', async (req, res) => {
+  const { username, password, email, avatar } = req.body
   try {
-    const salt = bcrypt.genSaltSync();
+    const salt = bcrypt.genSaltSync()
     const newUser = await new User({
       username,
       password: bcrypt.hashSync(password, salt),
       email,
       avatar,
-    }).save();
+    }).save()
 
     res.status(201).json({
       response: {
@@ -25,17 +24,17 @@ userRouter.post("/signup", async (req, res) => {
         avatar: newUser.avatar,
       },
       success: true,
-    });
+    })
   } catch (error) {
-    res.status(400).json({ response: error, success: false });
+    res.status(400).json({ response: error, success: false })
   }
-});
+})
 
-userRouter.post("/signin", async (req, res) => {
-  const { username, password } = req.body;
+userRouter.post('/signin', async (req, res) => {
+  const { username, password } = req.body
 
   try {
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ username })
 
     if (user && bcrypt.compareSync(password, user.password)) {
       res.status(200).json({
@@ -47,16 +46,16 @@ userRouter.post("/signin", async (req, res) => {
           email: user.email,
         },
         success: true,
-      });
+      })
     } else {
       res.status(404).json({
         response: "Username or password doesn't match",
         success: false,
-      });
+      })
     }
   } catch (error) {
-    res.status(400).json({ response: error, success: false });
+    res.status(400).json({ response: error, success: false })
   }
-});
+})
 
-module.exports = userRouter;
+module.exports = userRouter
